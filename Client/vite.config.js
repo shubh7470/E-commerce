8ebb0716cfce86ce97974105c313d2c39b330124
shubh7470 <shubh7470@gmail.com>
2025-10-13 22:14:11 +0530

    route problem solve

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
 })
